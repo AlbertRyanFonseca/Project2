66d5ca28d637dfb7ea7f640c54a00787127c54b1
Mcewball13 <mcewen1984@hotmail.com>
2021-07-25 07:04:11 -0700

    starting exercise view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,28 +1,67 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const sequelize = require("../config/connection");
 
-router.get('/', (req, res) => {
-    res.render('homepage');
+router.get("/", (req, res) => {
+    res.render("homepage");
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
     if (req.session.loggedIn) {
-        res.redirect('/exercises');
+        res.redirect("/exercises");
         return;
     }
 
-    res.render('login');
+    res.render("login");
 });
 
-router.get('/register', (req, res) => {
-    res.render('register');
+router.get("/register", (req, res) => {
+    res.render("register");
 });
 
-router.get('/profile', (req, res) => {
-    res.render('profile');
+router.get("/profile", (req, res) => {
+    res.render("profile");
 });
 
-router.get('/exercises', (req, res) => {
-    res.render('exercises');
+router.get("/exercises", (req, res) => {
+    Post.findAll({
+        attributes: [
+            "id",
+            "title",
+            "description",
+            "img_url",
+            "created_at",
+            // [
+            //     sequelize.literal(
+            //         "SELECT COUNT(*) FROM votes WHERE post.id = vote.post_id"
+            //     ),
+            //     "vote_count",
+            // ],
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "user_id",
+                    "post_id",
+                    "created_at",
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
+            { model: User, attributes: ["username"] },
+        ],
+    }).then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}))
+        res.render("exercises", {posts});
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
