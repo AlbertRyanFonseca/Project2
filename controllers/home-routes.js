4aec45a605fa92d2da9b7dfc801de05f32ab33ee
Mcewball13 <mcewen1984@hotmail.com>
2021-07-30 16:14:31 -0700

    created id for card id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -227,6 +227,69 @@ router.get("/exercises", (req, res) => {
             res.status(500).json(err);
         });
 });
+router.get("/post/:id", (req, res) => {
+    Post.findAll({
+        where: { 
+            id: req.params.id,
+        },
+        attributes: [
+            "id",
+            "title",
+            "description",
+            "created_at",
+            [
+                sequelize.literal(
+                    "(SELECT COUNT(*) FROM votes WHERE post.id = votes.post_id)"
+                ),
+                "vote_count",
+            ],
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    "id",
+                    "comment_text",
+                    "user_id",
+                    "post_id",
+                    "created_at",
+                    [
+                        sequelize.literal(
+                            "(SELECT COUNT(*) FROM comment WHERE post.id = comment.post_id)"
+                        ),
+                        "comment_count",
+                    ],
+                ],
+                include: {
+                    model: User,
+                    attributes: ["username"],
+                },
+            },
+            {
+                model: User,
+                attributes: ["username"],
+            },
+            {
+                model: Picture,
+                attributes: ["image_url"],
+            },
+        ],
+    })
+        .then((dbPostData) => {
+            const posts = dbPostData.map((post) => {
+                post.dataValues.loggedIn = req.session.loggedIn;
+                return post.get({ plain: true });
+            });
+            res.render("post", {
+                posts,
+                loggedIn: req.session.loggedIn,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // router.get("/type", (req, res) => {
 //     Type.findAll({
